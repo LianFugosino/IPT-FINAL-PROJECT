bef81403ea9462f5616d606982073be589ee7d9a
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2025-05-20 08:15:57 +0800

    added api routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Http\Controllers\Api\ApiKeyController;
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\BorrowRecordController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::resource('users', UserController::class);
+Route::resource('books', UserController::class);
+Route::resource('borrow_records', BorrowRecordController::class);
+Route::resource('api_keys', ApiKeyController::class);
